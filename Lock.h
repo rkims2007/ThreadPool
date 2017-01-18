889cca44ef2c9067ac8a73affe328f7ce110bb38
krishna <rkims.2007@gmail.com>
2017-01-18 17:50:36 +0530

    Scoped Lock

--- Lock.h
+++ Lock.h
@@ -1,19 +1,41 @@
-#ifndef LOCK_INCLUDED
-#define LOCK_INCLUDED
-
-#include<pthread>
-
-class Mutex
-{
-private:
-    pthraed_mutex_t  m_Mutex;
-    bool             m_bIsLocked;
-public:
-    Mutex();
-    bool Lock();
-    bool UnLock();
-    bool IsLocked();
-
-};
-
-#endif // LOCK_INCLUDED
+0a1,40
+> #ifndef LOCK_INCLUDED
+> #define LOCK_INCLUDED
+> 
+> #include<pthread>
+> 
+> class Mutex
+> {
+> private:
+>     pthraed_mutex_t  m_Mutex;
+>     bool             m_bIsLocked;
+> public:
+>     Mutex();
+>     bool Lock();
+>     bool UnLock();
+>     bool IsLocked();
+> 
+> };
+> 
+> class ScopedLock
+> {
+> private:
+>     const Mutex * m_pScopedLock
+> public:
+>         ScopedLock(const Mutex *ptr):m_pScopedLock(ptr)
+>         {
+>             if(m_pScopedLock)
+>             {
+>                 m_pScopedLock ->Lock();
+>             }
+>         }
+>         ~ScopedLock()
+>         {
+>             if(m_pScopedLock)
+>             {
+>                 m_pScopedLock->UnLock();
+>             }
+>         }
+> };
+> 
+> #endif // LOCK_INCLUDED
